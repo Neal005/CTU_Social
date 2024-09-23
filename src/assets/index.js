3b2df6928c1df317bdc1e8a4a36621908cb5d9d7
Neal Nguyen <hluc2003@gmail.com>
2024-09-24 02:01:36 +0700

    deleted

--- src/assets/index.js
+++
@@ -1,3 +0,0 @@
-import NoProfile from "./userprofile.png";
-import BgImage from "./CTU-Logo.png";
-export { NoProfile, BgImage };
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Login from "./Login";
-import Home from "./Home";
-import Register from "./Register";
-import Profile from "./Profile";
-import ResetPassword from "./ResetPassword";
-// import ImageDetail from "./ImageDetail";
-
-export { Login, Home, Register, Profile, ResetPassword };
