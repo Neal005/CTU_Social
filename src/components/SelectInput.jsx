497f7d2fc8c10735f063fd09418e4155c6365b1c
Neal Nguyen <hluc2003@gmail.com>
2024-09-24 02:08:12 +0700

    github page deploy

---
+++ src/components/SelectInput.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const SelectInput = React.forwardRef(
+  ({ label, labelStyles, options, value, onChange, styles, error }, ref) => {
+    return (
+      <div className="w-full flex flex-col mt-2">
+        {label && (
+          <p className={`text-ascent-2 text-sm mb-2 ${labelStyles}`}>
+            {<span className="font-bold">
+              {label}
+            </span>}
+          </p>
+        )}
+
+        <div>
+          <select
+            ref={ref}
+            value={value}
+            onChange={onChange}
+            className={`bg-secondary rounded border border-[#66666690] mb-2 outline-none text-sm text-ascent-2 px-4 py-3 placeholder:text-[#666] ${styles}`}
+            aria-invalid={error ? "true" : "false"}
+          >
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        </div>
+        {error && (
+          <span className="text-xs text-[#f64949fe] mt-0.5 ">{error}</span>
+        )}
+      </div>
+    );
+  }
+);
+
+export default SelectInput;
